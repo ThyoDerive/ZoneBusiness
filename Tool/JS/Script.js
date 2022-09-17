5833770960c8893f76be5e39b958ff35685d31b4
ThyoDerive <derivetheo96@gmail.com>
2022-09-17 13:30:37 +0200

    All Home Page Finish

--- Tool/JS/Script.js
+++ Tool/JS/Script.js
@@ -1,4 +1,7 @@
 const Onglet = document.querySelectorAll('.Onglet')
+const ResauxInstagram = document.getElementById('InstagramContent')
+const ResauxDiscord = document.getElementById('DiscordContent')
+const ResauxTwitter = document.getElementById('TwitterContent')
 
 Onglet.forEach((i) => {
     i.addEventListener('click', (e) => {
@@ -35,4 +38,28 @@ Onglet.forEach((i) => {
                 break;
         }
     })
+})
+
+ResauxInstagram.addEventListener('mouseover', (e) => {
+    ResauxInstagram.classList.add('ResauxActive')
+})
+
+ResauxInstagram.addEventListener('mouseleave', (e) => {
+    ResauxInstagram.classList.remove('ResauxActive')
+})
+
+ResauxDiscord.addEventListener('mouseover', (e) => {
+    ResauxDiscord.classList.add('ResauxActive')
+})
+
+ResauxDiscord.addEventListener('mouseleave', (e) => {
+    ResauxDiscord.classList.remove('ResauxActive')
+})
+
+ResauxTwitter.addEventListener('mouseover', (e) => {
+    ResauxTwitter.classList.add('ResauxActive')
+})
+
+ResauxTwitter.addEventListener('mouseleave', (e) => {
+    ResauxTwitter.classList.remove('ResauxActive')
 })
